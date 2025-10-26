e05a4fe118cd33c6da462e4dc853cefd4633e59d
Yusuf <yusufdgrl72@gmail.com>
2025-10-26 18:10:31 +0300

    Prepare project for Vercel deployment (static files collected)

--- backend/coffee_backend/settings.py
+++ backend/coffee_backend/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-change-in-pro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
